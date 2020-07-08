4f8c5dccdc87309ee78182c2bc92911d3aa45bc3
Allan Freitas <allan.cassio1@gmail.com>
2020-07-08 02:31:12 -0300

    Conluido o desafio 4.1, ulizar box-sizing, +, transition e after no header

---
+++ README.md
@@ -0,0 +1,6 @@
+# DESAFIO PROPOSTOS
+
+## DESAFIO 4.1
+
+Criar o "header" com as mesmas funcionalidades da aula. Usar o conceitos de "box-sinzing", o seletor "+".
+E utilizar o "After" e "Transition" para dar efeitos em links.
--- server.js
+++ server.js
@@ -1,53 +1,19 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
 
 const server = express()
-const videos = require("./data")
 
 server.use(express.static('public'))
-
+server.use(routes)
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server,
-    autoescape: false,
-    noCache: true
-})
-
-server.get("/about", function(request, response){
-    const about = {
-        avatarUrl: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-        name: "Mayk Brito",
-        role: "FullStack Developer",
-        description: 'Instrutor na <a href="https://rocketseat.com.br/" target="_blanck">&copy;Rocketseat</a> e colaborador na comunidade JavaScript.',
-        links: [
-            {name: "GitHub", url: "https://github.com/maykbrito"},
-            {name: "Linkedin", url: "https://www.linkedin.com/in/maykbrito/"},
-            {name: "Twitter", url: "https://twitter.com/maykbrito"}
-        ]
-    }
-
-    return response.render("about", {about})
-})
-server.get("/portifolio", function(request, response){
-    return response.render("portifolio", {itens: videos})
-})
-server.get("/video", function(request, response){
-    const id = request.query.id
-    const video = videos.find(function(video){
-        // return video.id == id // esta é a maneira mais rapida de retornar true ou false
-
-        if(video.id == id){
-            return true
-        }
-        // esta maneira mais facil de entender p return true ou false.
-    })
-    if(!video){
-        return response.send("Video not found!")
-    }
-    return response.render("video", {item: video})
+  express:server,
+  autoescape: false,
+  noCache: true
 })
 
 server.listen(5000, function(){
-    console.log('servidor está rodando / server is running')
+  console.log('server is running')
 })
