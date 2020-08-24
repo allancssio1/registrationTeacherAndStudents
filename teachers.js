ad2a4959c9cdc99be28b14d728c1855b03674828
Allan Freitas <allan.cassio1@gmail.com>
2020-08-24 15:27:51 -0300

    created put and delete routes and their settings

--- teachers.js
+++ teachers.js
@@ -2,6 +2,41 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, graduation, modalidad, date } = require('./utils')
 const Intl = require('intl')
+const { prototype } = require('stream')
+
+exports.post = function (req, res){
+  const keys = Object.keys(req.body)
+  
+  for (key of keys) {
+    if (req.body[key] == ""){
+      return res.send('Preencha todos os campos!')
+    }
+  }
+  
+  let { avatar_url, name, birth, level, area, model } = req.body
+  
+  birth = Date.parse(birth)
+  const create_at = Date.now()
+  const id = Number(data.teachers.length + 1)
+  
+  data.teachers.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    create_at,
+    level,
+    area,
+    create_at,
+    model
+  })
+  
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send ('Erro no documento')
+    
+    return res.redirect(`/teachers/${id}`)
+  })
+}
 
 exports.show = function (req, res) {
   const { id } = req.params
@@ -41,36 +76,48 @@ exports.edit = function (req,res) {
   return res.render('teachers/edit', { teacher })
 }
 
-exports.post = function (req, res){
-  const keys = Object.keys(req.body)
+exports.put = function (req, res) {
 
-  for (key of keys) {
-    if (req.body[key] == ""){
-      return res.send('Preencha todos os campos!')
+  const {id} = req.body
+  let index = 0
+  const foundTeacher = data.teachers.find(function(teacher, foundIndex){
+    if (teacher.id == id){
+      index = foundIndex
+      return true
     }
-  }
-  
-  let { avatar_url, name, birth, level, area, model } = req.body
-
-  birth = Date.parse(birth)
-  const create_at = Date.now()
-  const id = Number(data.teachers.length + 1)
-
-  data.teachers.push({
-    id,
-    avatar_url,
-    name,
-    birth,
-    create_at,
-    level,
-    area,
-    create_at,
-    model
   })
+  if (!foundTeacher) return res.send('Teacher not found!')
+
+  const teacher = {
+    ...foundTeacher,
+    ...req.body,
+    id: Number(req.body.id),
+    birth: Date.parse(req.body.birth)
+  }
+
+  data.teachers[index] = teacher
 
   fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
-    if (err) return res.send ('Erro no documento')
+    if(err) return res.send ('Write Error')
+    
+    return res.redirect(`/teachers/${id}`)
+  })
+}
 
-    return res.redirect('teachers')
+exports.delete = function (req, res) {
+  const { id } = req.body
+  let index =  0
+  const filterTeacher = data.teachers.filter(function (teacher, foundIndex){
+    index = foundIndex
+    return teacher.id != id
+  })
+
+  data.teachers = filterTeacher
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
+    if (err) {
+      return res.send ('Write error')
+    }
+    return res.redirect ('/teachers')
   })
 }
